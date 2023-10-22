fa231332aa7a9d647cbefbdeca1ec43289cde70b
jaygrossman <jay@grossman.org>
2023-10-22 00:42:23 -0400

    js redir test

---
+++ assets/js/redirect.js
@@ -0,0 +1,7 @@
+
+var url=document.location.href.toLowerCase();
+
+if(url.indexOf('/post/2023/06/30/') > -1) 
+    document.location.href = '/creating-singer-target-to-send-data-to-web-endpoint';
+
+
